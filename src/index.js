1f00d4c412170d993ea5bf6e431ea8a726148ae2
Artur Duarte <arturduartemoraes@gmail.com>
2023-07-19 12:43:40 -0300

    ✨ feat: add get todos route

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ function checksExistsUserAccount(request, response, next) {
     });
   }
 
+  request.user = user;
+
   return next();
 }
 
@@ -44,7 +46,8 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
