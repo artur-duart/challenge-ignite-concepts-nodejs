63ebb7f3c39bcbf7c9692732d79bc587f211db5b
Artur Duarte <arturduartemoraes@gmail.com>
2023-07-19 13:18:30 -0300

    ✨ feat: add create todos route (post)

--- src/index.js
+++ src/index.js
@@ -51,7 +51,20 @@ app.get("/todos", checksExistsUserAccount, (request, response) => {
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { user } = request;
+
+  const todo = {
+    id: uuidv4(),
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date(),
+  };
+
+  user.todos.push(todo);
+
+  return response.status(200).send();
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
